791b9e920bc6797d2a180485840bb35479de171c
Koteswararao S <srkotesh03@gmail.com>
2026-03-30 15:12:26 +0530

    Fix MongoDB admin M2M primary keys

--- apps/academics/apps.py
+++ apps/academics/apps.py
@@ -2,5 +2,5 @@
 
 
 class AcademicsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django_mongodb_backend.fields.ObjectIdAutoField'
     name = 'apps.academics'
--- apps/audit/apps.py
+++ apps/audit/apps.py
@@ -2,5 +2,5 @@
 
 
 class AuditConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django_mongodb_backend.fields.ObjectIdAutoField'
     name = 'apps.audit'
